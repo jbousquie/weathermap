75b42984477a294793b84552274ef12d13ab16f7
jbousquie <jerome@bousquie.fr>
2014-12-05 16:08:10 +0100

    json fabriqué avec les noms de port, la vitesse, etc

--- weathermap.rb
+++ weathermap.rb
@@ -9,7 +9,7 @@
 # Le script est une boucle infinie qui, à intervalles réguliers refresh_rate (secondes), lance une collecte
 # des valeurs inOctects et outOctects sur la liste des ports du tableau @indexes et les stockes dans les tableaux @in et @out.
 # Il calcule après chaque collecte le delta avec la mesure précédente et le stocke dans un hash par index.
-# Chaque hash est inséré dans un tableau @ports, lui même associé à chaque @host du tableau @hosts
+# Chaque hash est inséré dans un tableau @port_names, lui même associé à chaque @host du tableau @hosts
 # Ce tableau est encodé en JSON est écrit dans un fichier publiable sur le web.
 
 
@@ -34,18 +34,24 @@ class Host
     @@in_octets_mib = "1.3.6.1.2.1.2.2.1.10"
     @@out_octets_mib = "1.3.6.1.2.1.2.2.1.16"
 
-    # la variable d'instance name est rendue lisible directement : host.name
-    attr_reader :name
+    # les variable d'instance name et monitor sont rendues lisibles directement
+    attr_reader :name, :monitor
 
-  # constructeur : nom, communauté snmp version protocole, adresse IP, tableau des noms de port à monitorer
-  def initialize(name, ip, community, version, port_names)
+  # constructeur : nom, coordonées 3D, coord 3D label, monitor (boolean : à monitorer ?), 
+  # ip, communauté snmp version protocole, adresse IP, tableau des noms de port à monitorer
+  def initialize(name, coord, label, monitor, ip=nil, community=nil, version=nil, port_names=nil)
     @name = name
-    @host = ip.to_s
-    @ports = port_names             # tableau des noms de ports provenant du fichier de configuration
-    @indexes = []                   # tableau des indexes d'interfaces de cet hôte
-    @interfaces = Hash.new          # hash des objets interfaces de cet hôte
-    connect(version, community)
-    init_interfaces
+    @coord = coord
+    @label = label
+    @monitor = monitor
+    if monitor then
+      @host = ip.to_s
+      @port_names = port_names        # hash des {noms de ports => destination} provenant du fichier de configuration
+      @indexes = []                   # tableau des indexes d'interfaces de cet hôte
+      @interfaces = Hash.new          # hash des objets interfaces de cet hôte
+      connect(version, community)
+      init_interfaces
+    end
   end
 
 
@@ -79,10 +85,13 @@ class Host
         ts_delta = @ts[index][1] - @ts[index][0]
         out_delta = (@out[index][1] - @out[index][0]) / ts_delta
         in_delta = (@in[index][1] - @in[index][0]) / ts_delta
-        
+
+        # récupération de l'objet Interface de cet index
+        iface = @interfaces[index]
+
         # création d'un hash de résultat de la mesure pour chaque index
-        # et ajout de ce hash dans le tableau @ports
-        port_delta = { idx: index, in_out: [in_delta, out_delta] }
+        # et ajout de ce hash dans le tableau @port_names
+        port_delta = { name: iface.name, descr: iface.descr, speed: iface.speed ,octInOut: [in_delta, out_delta] }
         @deltas.push(port_delta)
       else
         #  index == tmp_index, donc on lit la première ligne d'index => in
@@ -94,6 +103,7 @@ class Host
     end
     return @deltas 
   end
+
 
   # méthodes privées ##########################################
   private
@@ -133,38 +143,45 @@ class Host
     puts "\n#{@name} :"
     @manager.walk([@@oid_ifnames, @@oid_ifdescr, @@oid_ifspeed]) do |ifname, ifdescr, ifspeed|
       index = ifname.name.to_s.gsub("IF-MIB::ifName.","")
-      name = ifname.value
-      descr = ifdescr.value
-      speed = ifspeed.value
-      if @ports.include?(name)        # si l'interface fait partie des ports demandés dans la conf
+      name = ifname.value.to_s
+      descr = ifdescr.value.to_s
+      speed = ifspeed.value.to_i
+      if @port_names.has_key?(name)         # si l'interface fait partie des ports demandés dans la conf
         then
+          link = @port_names[name]
           @indexes.push(index)
-          @interfaces[index] = Interface.new(index, name, descr, speed)
-          puts "  got #{name}  index = #{index}   speed = #{speed}"
+          @interfaces[index] = Interface.new(index, name, descr, speed, link)
+          link ||=  "nothing"
+          puts "  got #{name}  index = #{index} speed = #{speed} linked to #{link}"
       end
     end
   end
 
 end
 
+
 # object Interface
 class Interface
-  attr_reader :index, :name, :descr, :speed
-  def initialize(index, name, descr, speed)
-    @index = index
-    @name = name
-    @descr = descr
-    @speed = speed
+  attr_reader :index, :name, :descr, :speed, :link
+  # constructeur 
+  def initialize(index, name, descr, speed, link)
+    @index = index                # index du port
+    @name = name                  # nom du port
+    @descr = descr                # description du port
+    @speed = speed                # vitesse du port
+    @link = link                  # nom de l'équipement relié au port
   end
 end
 
 
+
+# ===================
 # Programme principal
 # ===================
 
 # tableau des résultats de toutes les mesures de tous les équipements
 results = Hash.new
-# tableaux des objets Host
+# tableaux des objets Host à monitorer
 hosts = []
 
 # délai de collecte général
@@ -174,19 +191,32 @@ refresh_rate = 15
 # chargement du fichier de conf YAML 
 conf = YAML.load_file(file_yaml)
 conf.each_pair {|name, params|
-  ip = params["ip"]
-  community = params["community"]
-  protocol_version = params["version"].to_sym
-  ifnames = params["ifnames"]
-  host = Host.new(name, ip, community, protocol_version, ifnames)
+  name = name.strip
+  coord = params["coord"]
+  label = params["label"]
+  monitor = false
+  # si une adresse ip est configurée, on monitore
+  if params.has_key?("ip") 
+    then 
+    monitor = true
+    ip = params["ip"].strip
+    community = params["community"].strip
+    protocol_version = params["version"].to_sym
+    ifnames = params["ifnames"]
+  end
+  host = Host.new(name, coord, label, monitor, ip, community, protocol_version, ifnames)
   hosts.push(host)
 }
+
+
 # boucle de collecte
 puts "\nMonitoring started with refresh rate = #{refresh_rate} s"
 while true do
   # ici boucle sur tous les hosts de la conf à faire
   hosts.each{ |host|
-    results[host.name] = host.get_snmp
+    if host.monitor then
+      results[host.name] = host.get_snmp
+    end
   }
   file_json = File.new(file_json, "w")
   file_json.write(JSON.generate(results))
